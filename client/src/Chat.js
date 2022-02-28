feea3a0d4de5108d7f8bcfb9e13739a02ad7b837
Abdirahman Suldaan <suldaan.89@hotmail.com>
2022-02-28 22:05:45 +0200

    this file was replaced by Chatbox.js

--- client/src/Chat.js
+++ client/src/Chat.js
@@ -1,83 +1,85 @@
-import React, { useEffect, useState } from "react";
-import ScrollToBottom from "react-scroll-to-bottom";
+//ATTENTION !! this file was replaced by Chatbox.js and kept in case of misbehaviour of the new file!  //////
 
-function Chat({ socket, username, recipient }) {
-  const [currentMessage, setCurrentMessage] = useState("");
-  const [messageList, setMessageList] = useState([]);
+// import React, { useEffect, useState } from "react";
+// import ScrollToBottom from "react-scroll-to-bottom";
 
-  const sendMessage = async () => {
-    if (currentMessage !== "") {
-      const messageData = {
-        recipient: recipient,
-        author: username,
-        message: currentMessage,
-        time:
-          new Date(Date.now()).getHours() +
-          ":" +
-          new Date(Date.now()).getMinutes(),
-      };
+// function Chat({ socket, username, recipient }) {
+//   const [currentMessage, setCurrentMessage] = useState("");
+//   const [messageList, setMessageList] = useState([]);
 
-      //   await socket.emit("send_message", messageData);
-      await socket.emit("private message", {
-        content: messageData,
-        to: recipient.id,
-      });
-      setMessageList((list) => [...list, messageData]);
-      setCurrentMessage("");
-    }
-  };
+//   const sendMessage = async () => {
+//     if (currentMessage !== "") {
+//       const messageData = {
+//         recipient: recipient,
+//         author: username,
+//         message: currentMessage,
+//         time:
+//           new Date(Date.now()).getHours() +
+//           ":" +
+//           new Date(Date.now()).getMinutes(),
+//       };
 
-  //this will add the new message on the frontend side by updating the state on the messageList
-  useEffect(() => {
-    socket.on("private message", ({ content, from }) => {
-      setMessageList((list) => [...list, content]);
-    });
-  }, [socket]);
+//       //   await socket.emit("send_message", messageData);
+//       await socket.emit("private message", {
+//         content: messageData,
+//         to: recipient.id,
+//       });
+//       setMessageList((list) => [...list, messageData]);
+//       setCurrentMessage("");
+//     }
+//   };
 
-  return (
-    <div className="chat-window">
-      <div className="chat-header">
-        <p>Live Chat</p>
-      </div>
-      <div className="chat-body">
-        <ScrollToBottom className="message-container">
-          {messageList.map((messageContent, i) => {
-            return (
-              <div
-                key={i}
-                className="message"
-                id={username === messageContent.author ? "you" : "other"}
-              >
-                <div>
-                  <div className="message-content">
-                    <p>{messageContent.message}</p>
-                  </div>
-                  <div className="message-meta">
-                    <p id="time">{messageContent.time}</p>
-                    <p id="author">{messageContent.author}</p>
-                  </div>
-                </div>
-              </div>
-            );
-          })}
-        </ScrollToBottom>
-      </div>
-      <div className="chat-footer">
-        <input
-          type="text"
-          value={currentMessage}
-          placeholder="Hey..."
-          onChange={(event) => {
-            setCurrentMessage(event.target.value);
-          }}
-          onKeyPress={(event) => {
-            event.key === "Enter" && sendMessage();
-          }}
-        />
-        <button onClick={sendMessage}>&#9658;</button>
-      </div>
-    </div>
-  );
-}
+//   //this will add the new message on the frontend side by updating the state on the messageList
+//   useEffect(() => {
+//     socket.on("private message", ({ content, from }) => {
+//       setMessageList((list) => [...list, content]);
+//     });
+//   }, [socket]);
 
-export default Chat;
+//   return (
+//     <div className="chat-window">
+//       <div className="chat-header">
+//         <p>Live Chat</p>
+//       </div>
+//       <div className="chat-body">
+//         <ScrollToBottom className="message-container">
+//           {messageList.map((messageContent, i) => {
+//             return (
+//               <div
+//                 key={i}
+//                 className="message"
+//                 id={username === messageContent.author ? "you" : "other"}
+//               >
+//                 <div>
+//                   <div className="message-content">
+//                     <p>{messageContent.message}</p>
+//                   </div>
+//                   <div className="message-meta">
+//                     <p id="time">{messageContent.time}</p>
+//                     <p id="author">{messageContent.author}</p>
+//                   </div>
+//                 </div>
+//               </div>
+//             );
+//           })}
+//         </ScrollToBottom>
+//       </div>
+//       <div className="chat-footer">
+//         <input
+//           type="text"
+//           value={currentMessage}
+//           placeholder="Hey..."
+//           onChange={(event) => {
+//             setCurrentMessage(event.target.value);
+//           }}
+//           onKeyPress={(event) => {
+//             event.key === "Enter" && sendMessage();
+//           }}
+//         />
+//         <button onClick={sendMessage}>&#9658;</button>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default Chat;
