b2f48cc4af5a22d0dd4458df05968772ed7954c8
Abdirahman Suldaan <suldaan.89@hotmail.com>
2022-03-11 15:43:18 +0200

    fix: app.js enter key fixed to enter dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -1,33 +1,39 @@
+//importing all the necessary modules
 import "./App.css";
 import io from "socket.io-client";
-import { useState, useEffect, useMemo, useContext } from "react";
+import { useState, useEffect, useMemo } from "react";
 import Dashboard from "./Dashboard";
 import { UserContext } from "./UserContext";
 
+// socket.io frontend client that connects to the server. if you wish to deploy the app the URL string needs to be changed
 const socket = io.connect("http://localhost:3001");
 
+//the module that will exported to index.js and rendered.
 function App() {
+  // different state object some are shared across components such as user.
   const [username, setUsername] = useState("");
-
   const [connected, setConnected] = useState(false);
   const [user, setUser] = useState(null);
-
   const provideValue = useMemo(() => ({ user, setUser }), [user, setUser]);
 
+  //this function emits the user information to the server
   useEffect(() => {
-    if (connected && socket.connected) {
-      socket.emit("send_user", {
-        username: username,
-        id: socket.id,
-        peerid: null,
-      });
-      setUser({ username: username, id: socket.id, peerid: null });
+    if (!connected && !socket.connected) {
+      return;
     }
+    socket.emit("send_user", {
+      username: username,
+      id: socket.id,
+      peerid: null,
+    });
+    //user object will contain these properties they will also be saved in the constant array users in the backend server
+    setUser({ username: username, id: socket.id, peerid: null });
   }, [username, connected]);
 
+  // this will show the user the form to get connected to the application and be assigned with a username
   if (!connected) {
     return (
-      <div className="App">
+      <div>
         <h1>Enter your name</h1>
         <input
           type="text"
@@ -35,9 +41,14 @@ function App() {
           onChange={(event) => {
             setUsername(event.target.value);
           }}
+          onKeyPress={(event) => {
+            event.key === "Enter" && setConnected(true);
+          }}
         />
         <button
+          type="submit"
           onClick={() => {
+            // once connected state is true this form will not be shown
             setConnected(true);
           }}
         >
@@ -45,6 +56,8 @@ function App() {
         </button>
       </div>
     );
+    // once connected the Dashboard component will be rendered here.
+    // it's wrapped in the UserContext that will share the state objects.
   } else {
     return (
       <UserContext.Provider value={provideValue}>
