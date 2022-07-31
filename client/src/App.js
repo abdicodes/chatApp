a01f04e3886ccda1972a386d4a533eae91eb9175
Abdirahman Suldaan <suldaan.89@hotmail.com>
2022-07-31 15:34:05 +0300

    build version

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,9 @@
 //importing all the necessary modules
-import io from "socket.io-client";
-import { useState, useEffect, useMemo } from "react";
-import Dashboard from "./Dashboard";
-import { UserContext } from "./UserContext";
-import Styles from "./Styles";
+import io from 'socket.io-client'
+import { useState, useEffect, useMemo } from 'react'
+import Dashboard from './Dashboard'
+import { UserContext } from './UserContext'
+import Styles from './Styles'
 import {
   Button,
   TextField,
@@ -12,33 +12,33 @@ import {
   AppBar,
   Toolbar,
   Container,
-} from "@mui/material";
+} from '@mui/material'
 
 // socket.io frontend client that connects to the server. if you wish to deploy the app the URL string needs to be changed
-const socket = io.connect("http://localhost:3001");
+const socket = io.connect('/')
 
 //the module that will exported to index.js and rendered.
 function App() {
   // different state object some are shared across components such as user.
-  const [username, setUsername] = useState("");
-  const [connected, setConnected] = useState(false);
-  const [user, setUser] = useState(null);
-  const provideValue = useMemo(() => ({ user, setUser }), [user, setUser]);
+  const [username, setUsername] = useState('')
+  const [connected, setConnected] = useState(false)
+  const [user, setUser] = useState(null)
+  const provideValue = useMemo(() => ({ user, setUser }), [user, setUser])
 
-  const classes = Styles();
+  const classes = Styles()
 
   //this function emits the user information to the server
   useEffect(() => {
     if (connected && socket.connected) {
-      socket.emit("send_user", {
+      socket.emit('send_user', {
         username: username,
         id: socket.id,
         peerid: null,
-      });
+      })
       //user object will contain these properties they will also be saved in the constant array users in the backend server
-      setUser({ username: username, id: socket.id, peerid: null });
+      setUser({ username: username, id: socket.id, peerid: null })
     }
-  }, [username, connected]);
+  }, [username, connected])
 
   // this will show the user the form to get connected to the application and be assigned with a username
   if (!connected) {
@@ -49,7 +49,7 @@ function App() {
           <Toolbar className={classes.navbar}>
             <img src="./favicon.ico" alt="" width="50" />
             <Typography variant="h6">
-              {" "}
+              {' '}
               <pre> Bekos Learn new languages!</pre>
             </Typography>
           </Toolbar>
@@ -57,14 +57,14 @@ function App() {
         <main>
           <div className={classes.Container}>
             <Container maxWidth="sm">
-              {" "}
+              {' '}
               <TextField
                 align="center"
                 onChange={(event) => {
-                  setUsername(event.target.value);
+                  setUsername(event.target.value)
                 }}
                 onKeyPress={(event) => {
-                  event.key === "Enter" && setConnected(true);
+                  event.key === 'Enter' && setConnected(true)
                 }}
                 id="outlined-basic"
                 label="Enter your name"
@@ -75,7 +75,7 @@ function App() {
                 align="center"
                 onClick={() => {
                   // once connected state is true this form will not be shown
-                  setConnected(true);
+                  setConnected(true)
                 }}
                 variant="contained"
               >
@@ -85,7 +85,7 @@ function App() {
           </div>
         </main>
       </>
-    );
+    )
     // once connected the Dashboard component will be rendered here.
     // it's wrapped in the UserContext that will share the state objects.
   } else {
@@ -93,8 +93,8 @@ function App() {
       <UserContext.Provider value={provideValue}>
         <Dashboard socket={socket} />
       </UserContext.Provider>
-    );
+    )
   }
 }
 
-export default App;
+export default App
